3895a85ba832c5ea193c8837df0be412d0f4fa96
Oliver Steiner <oliver@mollo.ch>
2020-07-20 01:29:05 +0200

    Add Leadership Twig Vars

--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -14,8 +14,8 @@ use Drupal\mollo_utils\Utility\Helper;
  *  Bundle mollo_work
  * -----------------------------------------
  */
-class ArtistController extends ControllerBase {
-
+class ArtistController extends ControllerBase
+{
   // public  Vars for Twig Var Suggestion.
   // use in Template via:
   // {# @var artist \Drupal\mollo_event\Controller\ArtistController #}
@@ -80,6 +80,8 @@ class ArtistController extends ControllerBase {
 
   public $role_description;
 
+  public $leadership;
+
   /**
    * Getvars.
    *
@@ -138,7 +140,8 @@ class ArtistController extends ControllerBase {
    * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
    * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
-  public static function getVars($artist_id): array {
+  public static function getVars($artist_id): array
+  {
     $node = Drupal::entityTypeManager()
       ->getStorage('node')
       ->load($artist_id);
@@ -156,37 +159,37 @@ class ArtistController extends ControllerBase {
         $node,
         'field_mollo_voice_position',
         'mollo_voice_position',
-        TRUE
+        true
       );
       $function = Helper::getFieldValue(
         $node,
         'field_mollo_function',
         'mollo_function',
-        TRUE
+        true
       );
       $instrument = Helper::getFieldValue(
         $node,
         'field_mollo_instrument',
         'mollo_instrument',
-        TRUE
+        true
       );
       $position = Helper::getFieldValue(
         $node,
         'field_mollo_position',
         'mollo_position',
-        TRUE
+        true
       );
       $speciality = Helper::getFieldValue(
         $node,
         'field_mollo_speciality',
         'mollo_speciality',
-        TRUE
+        true
       );
       $country = Helper::getFieldValue(
         $node,
         'field_mollo_country',
         'mollo_country',
-        TRUE
+        true
       );
 
       // Build Variables Array
@@ -200,7 +203,7 @@ class ArtistController extends ControllerBase {
         'position' => $position,
         'speciality' => $speciality,
         'image' => $title_image,
-        'country' => $country,
+        'country' => $country
       ];
     }
 
@@ -229,7 +232,8 @@ class ArtistController extends ControllerBase {
    * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
    * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
-  public static function getRoleVars($role_id): array {
+  public static function getRoleVars($role_id): array
+  {
     $node = Drupal::entityTypeManager()
       ->getStorage('node')
       ->load($role_id);
@@ -245,7 +249,49 @@ class ArtistController extends ControllerBase {
         'id' => $role_id,
         'name' => $name,
         'description' => $description,
-        'artist_id' => $artist_id,
+        'artist_id' => $artist_id
+      ];
+    }
+
+    return [];
+  }
+
+  /**
+   * getLeaderShipVars.
+   *
+   *    Bundle mollo_leadership
+   * -----------------------------------------
+   *
+   *   General
+   *    - field_mollo_artist    ( Entity: mollo_artist )
+   *    - field_mollo_event_1   ( Entity: mollo_event )
+   *    - field_mollo_position ( Term: mollo_position)
+   *
+   *
+   *
+   * @param $leader_id
+   *
+   * @return array|string[]
+   *   Return Artist Twig Vars
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  public static function getLeaderShipVars($leader_id): array
+  {
+    $node = Drupal::entityTypeManager()
+      ->getStorage('node')
+      ->load($leader_id);
+
+    if (isset($node)) {
+      // Get Field Values
+      $position_id = Helper::getFieldValue($node, 'field_mollo_position');
+      $artist_id = Helper::getFieldValue($node, 'field_mollo_artist');
+
+      // Build Variables Array
+      return [
+        'id' => $leader_id,
+        'position_id' => $position_id,
+        'artist_id' => $artist_id
       ];
     }
 
@@ -260,7 +306,8 @@ class ArtistController extends ControllerBase {
    * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
    * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
-  public static function getArtistsFromEvent($event_id, $vocabularies): array {
+  public static function getArtistsFromEvent($event_id, $vocabularies): array
+  {
     $all_artists = [];
 
     // load all Node IDs from "mollo_artist" with mollo_event ID
@@ -270,7 +317,7 @@ class ArtistController extends ControllerBase {
       ->condition('type', 'mollo_artist')
       ->condition('field_mollo_event', $event_id)
       // Access
-      ->accessCheck(FALSE);
+      ->accessCheck(false);
 
     $artist_ids = $query->execute();
 
@@ -328,7 +375,7 @@ class ArtistController extends ControllerBase {
       if (count($artists_filtered) > 0) {
         $terms[] = [
           'name' => $term_name,
-          'artists' => $artists_filtered,
+          'artists' => $artists_filtered
         ];
       }
     }
@@ -357,11 +404,11 @@ class ArtistController extends ControllerBase {
     foreach ($artists as $artist) {
       foreach ($artist['function'] as $function) {
         // Needle in Function (Choir, Orchestra, Leadership)
-        if (in_array($needle, $artist['function'], TRUE)) {
+        if (in_array($needle, $artist['function'], true)) {
           // term in Voc ( Voice Position, Instruments
-          if (in_array($term_name, $artist[$vocabulary], TRUE)) {
+          if (in_array($term_name, $artist[$vocabulary], true)) {
             // Eliminate duplicates
-            if (!in_array($artist['id'], $ids, TRUE)) {
+            if (!in_array($artist['id'], $ids, true)) {
               $ids[] = $artist['id'];
 
               // Add filterd Artist to Result
@@ -390,7 +437,7 @@ class ArtistController extends ControllerBase {
       ->condition('type', 'mollo_role')
       ->condition('field_mollo_event_1', $event_id)
       // Access
-      ->accessCheck(FALSE);
+      ->accessCheck(false);
 
     $role_ids = $query->execute();
 
@@ -430,8 +477,7 @@ class ArtistController extends ControllerBase {
 
       if (count($role_group) === 1) {
         $solo_artists[0]['artists'][] = $role_group[0];
-      }
-      else {
+      } else {
         foreach ($role_group as $artist) {
           $solo_artists[$i]['name'] = $artist['role_name'];
           $solo_artists[$i]['artists'][] = $artist;
@@ -443,4 +489,59 @@ class ArtistController extends ControllerBase {
     return $solo_artists;
   }
 
+  public static function getLeaderShip(
+    int $event_id,
+    array $artists,
+    array $vocabularies
+  ) {
+    $leadership = [];
+    $solo_artists_temp = [];
+    $leaders = [];
+
+    // get all Roles for Event
+    $query = Drupal::entityQuery('node')
+      //
+      // Condition
+      ->condition('type', 'mollo_event_leadership')
+      ->condition('field_mollo_event_1', $event_id)
+      // Access
+      ->accessCheck(false);
+
+    $leader_ids = $query->execute();
+
+    // Nodes found
+    if (count($leader_ids) !== 0) {
+      // Load all Artists
+      foreach ($leader_ids as $leader_id) {
+        // Output Array
+        $leaders[] = self::getLeaderShipVars($leader_id);
+      }
+    }
+
+    // Add Artist to Roles
+    foreach ($leaders as $leader) {
+      // Search in Artist
+      foreach ($artists as $artist) {
+        // Find Artist for Role
+        if ($artist['id'] === $leader['artist_id']) {
+          // Get Name from Position Term
+          if (isset($leader['position_id'])) {
+            $artist['position'] =
+              $vocabularies['position'][
+                $leader['position_id']
+              ];
+          }
+
+          // Add Role-Vars to Artist
+          $artist['artist_id'] = $leader['id'];
+
+          // Store Artist in Solo Artists
+          // Group Artists by Roles ( like Soldaten, Zofen, Grissetten)
+          $leadership[] = $artist;
+        }
+      }
+    }
+
+    return $leadership;
+  }
 }
